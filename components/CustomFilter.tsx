b37fcb3ac8923b90d8eee25b3f23045b65e6308c
Ivan Raštegorac <ivan.rastegorac@ag04.com>
2023-07-31 21:51:21 +0200

    Added custom filter props

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+'useClient';
 
-const CustomFilter = () => {
+import { useState, Fragment } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { Listbox, Transition } from '@headlessui/react';
+import { CustomFilterProps } from '@/types';
+
+const CustomFilter = ({ title, options }: CustomFilterProps) => {
   return <div>CustomFilter</div>;
 };
 
--- types/index.ts
+++ types/index.ts
@@ -42,6 +42,16 @@ export interface SearchManufacturerProps {
   setManufacturer: (manufacturer: string) => void;
 }
 
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+}
+
 export interface CarProps {
   city_mpg: number;
   class: string;
